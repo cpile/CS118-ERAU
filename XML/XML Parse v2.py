c6d58d7d44e4d423ad4ba416e83d50fe8b2986fe
Chris Pile <pilec@my.erau.edu>
2021-04-28 01:13:04 -0700

    XML Parse v2 (potentially working)

--- XML/XML Parse v2.py
+++ XML/XML Parse v2.py
@@ -20,13 +20,34 @@ root = ET.fromstring(xmlstr)
 
 
 for report in root:  # parsing into first <Report> layer under <Reports>
-    #print(report.tag, report.attrib)
-    for prop in report: # parsing into parent list of <Prop>
+    test_type = report.get('Type')
+    uut_result = report.get('UUTResult')
+    for prop in report:  # parsing into parent list of <Prop>
         if prop[0].get('Name') == 'SerialNumber':
             serial_number = prop[0][0].text
-            print(f"Serial Number: {serial_number}")
         elif prop.get('Name') == 'StationInfo':
             station_id = prop[0][0].text
-            print(f"Station ID: {station_id}")
-            operator = prop[0][1].text
-            print(f"Operator: {operator}")
+            operator = prop[1][0].text
+        elif prop.get('Type') == 'TEResult':
+            if prop[0].get('Name') == 'TS':
+                if prop[0][0].get('Name') == 'SequenceCall':
+                    if prop[0][0][0].get('Name') == 'ResultList':
+                        for value in prop[0][0][0]:
+                            if value.get('ID') == '[0]':
+                                for subprop in value:
+                                    if subprop.get("Type") == "TEResult":
+                                        for subsubprop in subprop:
+                                            if subsubprop.get('Name') == 'ReportText':
+                                                x = subsubprop[0].text
+                                                y = x.replace('<b>', '\'')
+                                                y = y.replace('</b>', '')
+                                                y = y.replace('\n', '')
+                                                z = y.split("'")
+                                                uut_part = z[2]
+
+print(f"Test Type: {test_type}")
+print(f"UUT Result: {uut_result}")
+print(f"Serial Number: {serial_number}")
+print(f"Station ID: {station_id}")
+print(f"Operator: {operator}")
+print(uut_part)
